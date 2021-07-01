764c215c02cf86e97314adf8348d279505becfaf
Xavier Belanche <xbelanch@protonmail.com>
2021-07-01 08:25:15 +0200

    Move from pacman to yay for upgrading and mantain the package system

--- .update-system-packages.sh
+++ .update-system-packages.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+upgrade_arch_system() {
+    printf "\nUpgrading Arch system with YAY...\n"
+    yay -Yc
+    yay -Syu --devel --timeupdate
+    yay -Ps
+    printf "...Done upgrading the system\n"
+}
+
 if [ -f "/etc/arch-release" ]; then
-    echo "Arch update packages"
-    sudo pacman -Syyuu && yay && npm update
+    upgrade_arch_system
+    npm outdated -g
 else
     echo "Debian update packages"
-    sudo apt-get update && sudo apt-get -y upgrade && npm update
+    sudo apt-get update && sudo apt-get -y upgrade && npm outdated -g
 fi
