753475a4243c5fe6663e6387b4becaf064a2ec07
Xavier Belanche <xbelanch@protonmail.com>
2021-07-01 08:51:26 +0200

    npm check outdaded packages as a function

--- .update-system-packages.sh
+++ .update-system-packages.sh
@@ -14,9 +14,21 @@ upgrade_debian_system() {
     printf "...Done upgrading the system\n"
 }
 
+check_outdated_npm_packages() {
+    if which npm 2>&1 >/dev/null; then
+        printf "\nCheck outdated global npm installed packages\n"
+        npm outdated -g
+    else
+           printf "\nNPM is not installed on your system\n"
+    fi
+}
+
+# Update system
 if [ -f "/etc/arch-release" ]; then
     upgrade_arch_system
-    npm outdated -g
 else
     upgrade_debian_system
 fi
+
+# Check outdated npm installed global packages
+check_outdated_npm_packages
